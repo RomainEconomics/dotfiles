ed29f1cea9b51213fabeadca401372954a212cd7
JR <romain.jouhameau@icebergdatalab.com>
2025-03-09 10:26:43 +0100

    allow to toggle/untoggle formatting for single buffer

--- .config/nvim/lua/plugins/editor/conform.lua
+++ .config/nvim/lua/plugins/editor/conform.lua
@@ -9,6 +9,36 @@ return { -- Autoformat
 			mode = '',
 			desc = '[F]ormat buffer',
 		},
+		{
+			'<leader>uf',
+			function()
+				-- If autoformat is currently disabled for this buffer,
+				-- then enable it, otherwise disable it
+				if vim.b.disable_autoformat then
+					vim.b.disable_autoformat = false
+					vim.notify('Enabled autoformat for current buffer')
+				else
+					vim.b.disable_autoformat = true
+					vim.notify('Disabled autoformat for current buffer')
+				end
+			end,
+			desc = 'Toggle autoformat for current buffer',
+		},
+		-- {
+		-- 	'<leader>uF',
+		-- 	function()
+		-- 		-- If autoformat is currently disabled globally,
+		-- 		-- then enable it globally, otherwise disable it globally
+		-- 		if vim.g.disable_autoformat then
+		-- 			vim.cmd('FormatEnable')
+		-- 			vim.notify('Enabled autoformat globally')
+		-- 		else
+		-- 			vim.cmd('FormatDisable')
+		-- 			vim.notify('Disabled autoformat globally')
+		-- 		end
+		-- 	end,
+		-- 	desc = 'Toggle autoformat globally',
+		-- },
 	},
 	opts = {
 		notify_on_error = false,
